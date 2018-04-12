6a5735913f1eb5d1e1186167b896301d4ccd6040
Nikita <niagar@microsoft.com>
2018-04-12 12:08:30 +0530

    File change in the config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 let config = {}
 
-config.host = "https://allshipment-eastus-sit-db.documents.azure.com:443/";
-config.authKey = "hK6reowTOhAZkD7g4N6CqypD7dPWlK7LeLcirZvmHoMPvdZcVrVe43Vqyu2tY6nqSKf1VwDFODFDdWDKMCTJKw==";
-config.databaseId = "AllShipment";
-config.collectionId = "AllShipmentMetadata";
+config.host = "";
+config.authKey = "";
+config.databaseId = "";
+config.collectionId = "";
 
 module.exports = config;
